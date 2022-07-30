a6c631d154e0ee65fdd64176ac841ca8a1552cc3
kieferjackson <kieferleejackson@gmail.com>
2022-07-30 16:35:26 -0700

    Added fetch function and searchable cities

--- assets/js/city_data.js
+++ assets/js/city_data.js
@@ -1,7 +1,7 @@
 
 class City 
 {
-    constructor (name, longitude, latitude) 
+    constructor (name, latitude, longitude) 
     {
         this.name       = name;
         this.latitude   = latitude;
@@ -13,7 +13,27 @@ class City
 let cities = 
 {
     'New York': 
-        new City('New York', 40.7128, 74.0060),
+        new City('New York', 40.7128, -74.0060),
     'London':
-        new City('London', 51.5072, 0.1276)
+        new City('London', 51.5072, -0.1276),
+    'Boston':
+        new City('Boston', 42.3611, -71.0571),
+    'Paris': 
+        new City('Paris', 48.8566, 2.3522),
+    'Phoenix':
+        new City('Phoenix', 33.4483, -112.0740),
+    'Tokyo':
+        new City('Tokyo', 35.6762, 139.6503),
+    'Austin': 
+        new City('Austin', 30.2667, -97.7333),
+    'Chicago':
+        new City('Chicago', 41.8818, -87.6232),
+    'San Francisco':
+        new City('San Francisco',  37.7338, -122.4467),
+    'Seattle': 
+        new City('Seattle', 40.7128, -122.3351),
+    'Denver':
+        new City('Denver', 39.7420, -104.9915),
+    'Atlanta':
+        new City('Atlanta', 33.7537, -84.3863),
 }
--- assets/js/script.js
+++ assets/js/script.js
@@ -6,4 +6,28 @@ const CITY_FIELD = document.querySelector('#city_input');
 function search_city() 
 {
     let sel_city = CITY_FIELD.value;
+
+    let lat_coord = cities['New York'].latitude;
+    let lon_coord = cities['New York'].longitude;
+
+    let forecast_url = `${OW_URL}?lat=${lat_coord}&lon=${lon_coord}&units=imperial&appid=${API_KEY}`;
+
+    get_weather_forecast(forecast_url);
+}
+
+
+
+function get_weather_forecast(requested_url) 
+{
+fetch(requested_url)
+    .then( (response) => {
+    // Log the requested url address
+    console.log(`Data from: ${requested_url}`);
+    
+    return response.json();
+    })
+    .then( (data) => {
+    console.log(data);
+
+    });
 }
